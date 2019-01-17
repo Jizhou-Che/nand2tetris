1a7868b391f165dd7a6c0c91ee177b14308bbe5f
Jizhou Che <che_mail@icloud.com>
2019-01-17 15:36:22 +0800

    Output file created.

--- projects/06/Assembler.c
+++ projects/06/Assembler.c
@@ -51,10 +51,51 @@ int string_append(char ** str, char chr){
 	}
 }
 
+// Concatenates string str1 and string str2.
+// Returns a pointer to the destination string.
+// Returns NULL on memory allocation failure.
+char * string_concatenate(char * str1, char * str2){
+	int size = (int)(((strlen(str1) + strlen(str2)) / 16 + 1) * 16);
+	char * dst = malloc(size * sizeof(char));
+	if(dst == NULL){
+		return NULL;
+	}else{
+		int flag = 0;
+		int subFlag = 0;
+		while(str1[subFlag] != '\0'){
+			dst[flag] = str1[subFlag];
+			flag++;
+			subFlag++;
+		}
+		subFlag = 0;
+		while(str2[subFlag] != '\0'){
+			dst[flag] = str2[subFlag];
+			flag++;
+			subFlag++;
+		}
+		dst[++flag] = '\0';
+		return dst;
+	}
+}
+
+// Checks whether a given string is a symbol in the symbol table.
+// Returns the respective address of the symbol if it is already in the symbol table.
+// Returns -1 if the symbol is not in the symbol table.
+int symbol_table_check(struct symbol_table * headSymbol, char * s){
+	struct symbol_table * thisSymbol = headSymbol;
+	while(thisSymbol != NULL){
+		if(strcmp(thisSymbol->symbol, s) == 0){
+			return thisSymbol->address;
+		}
+		thisSymbol = thisSymbol->next;
+	}
+	return -1;
+}
+
 // Adds a new entry to the symbol table.
 // Returns 0 on memory allocation failure, 1 on success.
 // Actually performing a list_push.
-int symbol_table_add_entry(struct symbol_table ** headSymbol, char * s, int addr){
+int symbol_table_push(struct symbol_table ** headSymbol, char * s, int addr){
 	struct symbol_table * new_node = malloc(sizeof(struct symbol_table));
 	if(new_node == NULL){
 		return 0;
@@ -88,117 +129,117 @@ void symbol_table_clear(struct symbol_table ** headSymbol){
 // Returns NULL on memory allocation failure.
 struct symbol_table * symbol_table_initialize(){
 	struct symbol_table * symbolTable = NULL;
-	if(!symbol_table_add_entry(&symbolTable, "SP", 0)){
+	if(!symbol_table_push(&symbolTable, "SP", 0)){
 		printf("Assembly failed while allocating memory.\n");
 		symbol_table_clear(&symbolTable);
 		return NULL;
 	}
-	if(!symbol_table_add_entry(&symbolTable, "LCL", 1)){
+	if(!symbol_table_push(&symbolTable, "LCL", 1)){
 		printf("Assembly failed while allocating memory.\n");
 		symbol_table_clear(&symbolTable);
 		return NULL;
 	}
-	if(!symbol_table_add_entry(&symbolTable, "ARG", 2)){
+	if(!symbol_table_push(&symbolTable, "ARG", 2)){
 		printf("Assembly failed while allocating memory.\n");
 		symbol_table_clear(&symbolTable);
 		return NULL;
 	}
-	if(!symbol_table_add_entry(&symbolTable, "THIS", 3)){
+	if(!symbol_table_push(&symbolTable, "THIS", 3)){
 		printf("Assembly failed while allocating memory.\n");
 		symbol_table_clear(&symbolTable);
 		return NULL;
 	}
-	if(!symbol_table_add_entry(&symbolTable, "THAT", 4)){
+	if(!symbol_table_push(&symbolTable, "THAT", 4)){
 		printf("Assembly failed while allocating memory.\n");
 		symbol_table_clear(&symbolTable);
 		return NULL;
 	}
-	if(!symbol_table_add_entry(&symbolTable, "R0", 0)){
+	if(!symbol_table_push(&symbolTable, "R0", 0)){
 		printf("Assembly failed while allocating memory.\n");
 		symbol_table_clear(&symbolTable);
 		return NULL;
 	}
-	if(!symbol_table_add_entry(&symbolTable, "R1", 1)){
+	if(!symbol_table_push(&symbolTable, "R1", 1)){
 		printf("Assembly failed while allocating memory.\n");
 		symbol_table_clear(&symbolTable);
 		return NULL;
 	}
-	if(!symbol_table_add_entry(&symbolTable, "R2", 2)){
+	if(!symbol_table_push(&symbolTable, "R2", 2)){
 		printf("Assembly failed while allocating memory.\n");
 		symbol_table_clear(&symbolTable);
 		return NULL;
 	}
-	if(!symbol_table_add_entry(&symbolTable, "R3", 3)){
+	if(!symbol_table_push(&symbolTable, "R3", 3)){
 		printf("Assembly failed while allocating memory.\n");
 		symbol_table_clear(&symbolTable);
 		return NULL;
 	}
-	if(!symbol_table_add_entry(&symbolTable, "R4", 4)){
+	if(!symbol_table_push(&symbolTable, "R4", 4)){
 		printf("Assembly failed while allocating memory.\n");
 		symbol_table_clear(&symbolTable);
 		return NULL;
 	}
-	if(!symbol_table_add_entry(&symbolTable, "R5", 5)){
+	if(!symbol_table_push(&symbolTable, "R5", 5)){
 		printf("Assembly failed while allocating memory.\n");
 		symbol_table_clear(&symbolTable);
 		return NULL;
 	}
-	if(!symbol_table_add_entry(&symbolTable, "R6", 6)){
+	if(!symbol_table_push(&symbolTable, "R6", 6)){
 		printf("Assembly failed while allocating memory.\n");
 		symbol_table_clear(&symbolTable);
 		return NULL;
 	}
-	if(!symbol_table_add_entry(&symbolTable, "R7", 7)){
+	if(!symbol_table_push(&symbolTable, "R7", 7)){
 		printf("Assembly failed while allocating memory.\n");
 		symbol_table_clear(&symbolTable);
 		return NULL;
 	}
-	if(!symbol_table_add_entry(&symbolTable, "R8", 8)){
+	if(!symbol_table_push(&symbolTable, "R8", 8)){
 		printf("Assembly failed while allocating memory.\n");
 		symbol_table_clear(&symbolTable);
 		return NULL;
 	}
-	if(!symbol_table_add_entry(&symbolTable, "R9", 9)){
+	if(!symbol_table_push(&symbolTable, "R9", 9)){
 		printf("Assembly failed while allocating memory.\n");
 		symbol_table_clear(&symbolTable);
 		return NULL;
 	}
-	if(!symbol_table_add_entry(&symbolTable, "R10", 10)){
+	if(!symbol_table_push(&symbolTable, "R10", 10)){
 		printf("Assembly failed while allocating memory.\n");
 		symbol_table_clear(&symbolTable);
 		return NULL;
 	}
-	if(!symbol_table_add_entry(&symbolTable, "R11", 11)){
+	if(!symbol_table_push(&symbolTable, "R11", 11)){
 		printf("Assembly failed while allocating memory.\n");
 		symbol_table_clear(&symbolTable);
 		return NULL;
 	}
-	if(!symbol_table_add_entry(&symbolTable, "R12", 12)){
+	if(!symbol_table_push(&symbolTable, "R12", 12)){
 		printf("Assembly failed while allocating memory.\n");
 		symbol_table_clear(&symbolTable);
 		return NULL;
 	}
-	if(!symbol_table_add_entry(&symbolTable, "R13", 13)){
+	if(!symbol_table_push(&symbolTable, "R13", 13)){
 		printf("Assembly failed while allocating memory.\n");
 		symbol_table_clear(&symbolTable);
 		return NULL;
 	}
-	if(!symbol_table_add_entry(&symbolTable, "R14", 14)){
+	if(!symbol_table_push(&symbolTable, "R14", 14)){
 		printf("Assembly failed while allocating memory.\n");
 		symbol_table_clear(&symbolTable);
 		return NULL;
 	}
-	if(!symbol_table_add_entry(&symbolTable, "R15", 15)){
+	if(!symbol_table_push(&symbolTable, "R15", 15)){
 		printf("Assembly failed while allocating memory.\n");
 		symbol_table_clear(&symbolTable);
 		return NULL;
 	}
-	if(!symbol_table_add_entry(&symbolTable, "SCREEN", 16384)){
+	if(!symbol_table_push(&symbolTable, "SCREEN", 16384)){
 		printf("Assembly failed while allocating memory.\n");
 		symbol_table_clear(&symbolTable);
 		return NULL;
 	}
-	if(!symbol_table_add_entry(&symbolTable, "KBD", 24576)){
+	if(!symbol_table_push(&symbolTable, "KBD", 24576)){
 		printf("Assembly failed while allocating memory.\n");
 		symbol_table_clear(&symbolTable);
 		return NULL;
@@ -213,7 +254,7 @@ int assembly_first_pass(FILE * inputFile, struct symbol_table ** headSymbol){
 	int lineNumber = -1;
 	char thisChar;
 	while(fscanf(inputFile, "%c", &thisChar) == 1){
-		if(thisChar == '\n' || thisChar == '\r' || thisChar == ' ' || thisChar == '\t'){
+		if(thisChar == '\r' || thisChar == '\n' || thisChar == ' ' || thisChar == '\t'){
 			// Empty line or space or tab.
 			continue;
 		}else if(thisChar == '/'){
@@ -224,15 +265,13 @@ int assembly_first_pass(FILE * inputFile, struct symbol_table ** headSymbol){
 				}
 			}
 		}else if(thisChar == '('){
-			// Beginning of label.
+			// Label line.
 			char * thisLabel = NULL;
 			while(fscanf(inputFile, "%c", &thisChar) == 1){
 				if(thisChar == ')'){
-					// End of label.
-					symbol_table_add_entry(headSymbol, thisLabel, lineNumber + 1);
+					symbol_table_push(headSymbol, thisLabel, lineNumber + 1);
 					break;
 				}else{
-					// Label character.
 					if(!string_append(&thisLabel, thisChar)){
 						free(thisLabel);
 						return 0;
@@ -246,13 +285,35 @@ int assembly_first_pass(FILE * inputFile, struct symbol_table ** headSymbol){
 				}
 			}
 		}else{
-			// Beginning of instruction.
+			// Instruction line.
 			lineNumber++;
 			while(fscanf(inputFile, "%c", &thisChar) == 1){
 				if(thisChar == '\n'){
 					break;
 				}
 			}
+		}
+	}
+	return 1;
+}
+
+// Second pass of the assembly process.
+// Returns 0 on memory allocation failure, 1 on success.
+int assembly_second_pass(FILE * inputFile, FILE * outputFile, struct symbol_table ** headSymbol){
+	char thisChar;
+	while(fscanf(inputFile, "%c", &thisChar) == 1){
+		if(thisChar == '\r' || thisChar == '\n' || thisChar == ' ' || thisChar == '\t'){
+			// Empty line or space or tab.
+			continue;
+		}else if(thisChar == '/' || thisChar == '('){
+			// Comment line or label line.
+			while(fscanf(inputFile, "%c", &thisChar) == 1){
+				if(thisChar == '\n'){
+					break;
+				}
+			}
+		}else{
+			// Instruction line.
 		}
 	}
 	return 1;
@@ -283,6 +344,25 @@ int main(int argc, char ** argv){
 		return 0;
 	}
 
-	printf("%s %d\n", symbolTable->symbol, symbolTable->address);
+	// Second pass of the assembly process.
+	fseek(inputFile, 0, SEEK_SET);
+	int flag = 0;
+	char * outputFileName = NULL;
+	while(argv[1][flag] != '.'){
+		string_append(&outputFileName, argv[1][flag]);
+		flag++;
+	}
+	outputFileName = string_concatenate(outputFileName, ".hack");
+	FILE * outputFile = fopen(outputFileName, "w");
+	if(outputFile == NULL){
+		printf("Cannot create file %s.\n", outputFileName);
+		return 0;
+	}
+	if(!assembly_second_pass(inputFile, outputFile, &symbolTable)){
+		printf("Assembly failed while allocating memory.\n");
+		return 0;
+	}
+
+	printf("Assembly successful.\n");
 	return 0;
 }
